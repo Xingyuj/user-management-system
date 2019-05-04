f3378e477db6099a9c6a3ba3aeba33d553ad5ac4
EthanXingyuJi <endorathas@gmail.com>
2019-05-04 15:05:29 +1000

    feat: add account controller to finish authentication service

---
+++ src/main/java/com/xingyu/controller/AccountController.java
@@ -0,0 +1,38 @@
+package com.xingyu.controller;
+
+import org.apache.shiro.authz.UnauthorizedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.xingyu.auth.JWTUtil;
+import com.xingyu.config.BaseResponse;
+import com.xingyu.model.UserAccount;
+import com.xingyu.service.UserAccountService;
+
+import io.swagger.annotations.Api;
+
+@Api(value = "Authenticator", protocols = "http")
+@RestController
+public class AccountController {
+
+    private UserAccountService userAccountService;
+
+    @Autowired
+    public void setService(UserAccountService userAccountService) {
+        this.userAccountService = userAccountService;
+    }
+    
+    @PostMapping("/login")
+    public BaseResponse<String> login(@RequestParam("username") String username,
+                              @RequestParam("password") String password) {
+        UserAccount userInfo = userAccountService.findByUsername(username);
+        if (userInfo.getPassword().equals(password)) {
+            return BaseResponse.successWithData(JWTUtil.sign(username, password));
+        } else {
+            throw new UnauthorizedException();
+        }
+    }
+    
+}
