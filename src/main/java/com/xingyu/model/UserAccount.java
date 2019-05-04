3ad3dc4cf81af3c295fe4978606adde51c924788
EthanXingyuJi <endorathas@gmail.com>
2019-05-04 14:54:27 +1000

    fix: model constraints

--- src/main/java/com/xingyu/model/UserAccount.java
+++ src/main/java/com/xingyu/model/UserAccount.java
@@ -2,7 +2,6 @@ package com.xingyu.model;
 
 import java.io.Serializable;
 import java.util.List;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -11,34 +10,20 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import javax.persistence.OneToOne;
 
 @Entity
 public class UserAccount implements Serializable {
 	private static final long serialVersionUID = -3210565389728151509L;
 	@Id
     @GeneratedValue
-    private Integer uid;
+    private Integer id;
     @Column(unique =true)
     private String username;
 	private String password;
-	
-	@OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "profile_id")
-    private UserProfile profile;
-
     private String salt;
     @ManyToMany(fetch= FetchType.EAGER)
     @JoinTable(name = "SysUserRole", joinColumns = { @JoinColumn(name = "uid") }, inverseJoinColumns ={@JoinColumn(name = "roleId") })
     private List<SysRole> roleList;
-
-    public Integer getUid() {
-        return uid;
-    }
-
-    public void setUid(Integer uid) {
-        this.uid = uid;
-    }
 
 	public String getUsername() {
         return username;
--- src/main/java/com/xingyu/model/UserProfile.java
+++ src/main/java/com/xingyu/model/UserProfile.java
@@ -9,6 +9,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.OrderBy;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -17,7 +18,12 @@ import javax.persistence.TemporalType;
 public class UserProfile {
 	@Id
     @GeneratedValue
-    private Integer profile_id;
+    private Integer id;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "uid")
+    private UserAccount account;
+	
 	private String firstname;
 	private String lastname;
 	@Temporal(TemporalType.DATE)
@@ -25,14 +31,21 @@ public class UserProfile {
 	private String email;
 	
     @OneToMany(cascade = CascadeType.ALL)
-    @JoinColumn(name = "uid")
+    @JoinColumn(name = "profile_id")
     @OrderBy("id asc")
     private Set<Address> addresses;
-	public Integer getProfile_id() {
-		return profile_id;
+    
+	public Integer getId() {
+		return id;
 	}
-	public void setProfile_id(Integer profile_id) {
-		this.profile_id = profile_id;
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public UserAccount getAccount() {
+		return account;
+	}
+	public void setAccount(UserAccount account) {
+		this.account = account;
 	}
 	public String getFirstname() {
 		return firstname;
