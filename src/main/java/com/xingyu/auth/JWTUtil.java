572e8f5ce4f287829e4d8c504268634dd5a35a11
EthanXingyuJi <endorathas@gmail.com>
2019-05-04 13:48:29 +1000

    feat: add JWT filter

---
+++ src/main/java/com/xingyu/auth/JWTUtil.java
@@ -0,0 +1,67 @@
+package com.xingyu.auth;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+
+public class JWTUtil {
+
+    // token expiration time
+    private static final long EXPIRE_TIME = 5*60*1000;
+
+    /**
+     * verify token
+     * @param token
+     * @param secret
+     * @return boolean
+     */
+    public static boolean verify(String token, String username, String secret) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withClaim("username", username)
+                    .build();
+            verifier.verify(token);
+            return true;
+        } catch (Exception exception) {
+            return false;
+        }
+    }
+
+    /**
+     * get user name from token
+     * @return
+     */
+    public static String getUsername(String token) {
+        try {
+            DecodedJWT jwt = JWT.decode(token);
+            return jwt.getClaim("username").asString();
+        } catch (JWTDecodeException e) {
+            return null;
+        }
+    }
+
+    /**
+     * to generate a token
+     * @param username
+     * @param secret
+     * @return String
+     */
+    public static String sign(String username, String secret) {
+        try {
+            Date date = new Date(System.currentTimeMillis()+EXPIRE_TIME);
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withClaim("username", username)
+                    .withExpiresAt(date)
+                    .sign(algorithm);
+        } catch (UnsupportedEncodingException e) {
+            return null;
+        }
+    }
+}
