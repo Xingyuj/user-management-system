c36641900aee9d1512048a098469664751487955
EthanXingyuJi <endorathas@gmail.com>
2019-05-04 23:30:55 +1000

    conf: enable swagger

---
+++ src/main/java/com/xingyu/config/SwaggerConfiguration.java
@@ -0,0 +1,49 @@
+package com.xingyu.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * swagger configuration
+ * @author
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+    @Bean
+    public Docket createRestApi(){
+        //add head start
+        ParameterBuilder tokenPar = new ParameterBuilder();
+        List<Parameter> pars = new ArrayList<>();
+        tokenPar.name("token").description("token").modelRef(new ModelRef("string")).parameterType("header").required(false).build();
+        pars.add(tokenPar.build());
+        //add head end
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.why.greenhouse"))
+                .paths(PathSelectors.any())
+                .build()
+                .globalOperationParameters(pars);
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("GREEN TOKEN SERVER API information")
+                .description("GREEN TOKEN SERVER API information")
+                .version("1.0")
+                .build();
+    }
+}
