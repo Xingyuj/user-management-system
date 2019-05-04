5d32283be8725a540949acdbe32761dbcd9f907b
EthanXingyuJi <endorathas@gmail.com>
2019-05-05 00:51:17 +1000

    feat: add dbm method for profile

--- src/main/java/com/xingyu/service/impl/UserProfileServiceImpl.java
+++ src/main/java/com/xingyu/service/impl/UserProfileServiceImpl.java
@@ -1,9 +1,12 @@
 package com.xingyu.service.impl;
 
+import java.util.ArrayList;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
+import com.google.common.collect.Lists;
 import com.xingyu.dao.UserProfileDao;
 import com.xingyu.model.UserAccount;
 import com.xingyu.model.UserProfile;
@@ -17,4 +20,38 @@ public class UserProfileServiceImpl implements UserProfileService {
     public UserProfile findByAccount(UserAccount account) {
         return userProfileDao.findByAccount(account);
     }
+
+    @Override
+    public boolean saveProfile(UserProfile account) {
+    	try {
+    		userProfileDao.save(account);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+        return true;
+    }
+    
+    @Override
+    public boolean deleteProfile(Long id) {
+    	try {
+    		userProfileDao.deleteById(id);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+        return true;
+    }
+    
+    @Override
+    public ArrayList<UserProfile> findAll() {
+    	ArrayList<UserProfile> arr = Lists.newArrayList(userProfileDao.findAll());
+    	System.out.println(arr);
+        return Lists.newArrayList(userProfileDao.findAll());
+    }
+
+	@Override
+	public UserProfile findById(long id) {
+        return userProfileDao.findById(id).get();
+	}
 }
