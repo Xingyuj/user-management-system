c64f6d5161db412b365ef1176c288a98f91d90d0
EthanXingyuJi <endorathas@gmail.com>
2019-05-05 20:59:10 +1000

    fix: insert sql may occur error as db not ready

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
-#!/bin/sh -x
+#!/bin/sh
+echo running docker container
 docker-compose up -d
+echo wait for db to get ready 
+sleep 5
+echo insert data into db
 docker cp ./dump.sql xingyu-postgres:/docker-entrypoint-initdb.d/dump.sql
 docker exec -u postgres xingyu-postgres psql postgres postgres -f docker-entrypoint-initdb.d/dump.sql
+echo done! ethan UMS is ready to play!
